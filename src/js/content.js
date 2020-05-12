55dd2f7133c53fe982cc40428fde3603532becea
issammani <issamouu69@gmail.com>
2020-05-12 15:23:20 +0000

    Add second selector for profile names

--- src/js/content.js
+++ src/js/content.js
@@ -1,21 +1,24 @@
 const PROFILE_DATA_SELECTOR = 'header section ul li';
-const PROFILE_NAME_SELECTOR = 'header section h2';
+const PROFILE_NAME_SELECTORS = ['header section h1', 'header section h2'];
 
 window.addEventListener('load', () => {
-
+    
     // Check if current page is a profile
     const isProfile = () => {
         const profileData = document.querySelectorAll(PROFILE_DATA_SELECTOR);
-        const profileName = document.querySelector(PROFILE_NAME_SELECTOR);
+        const profileName = PROFILE_NAME_SELECTORS.map(selector => document.querySelector(selector)).filter(v => v);
 
         // Check wether the current page is a profile page
-        if(profileData.length === 3){
+        if(profileName.length >= 1 && profileData.length === 3){
             chrome.runtime.sendMessage({body: 'profilePage', profile: {
-                name: profileName.textContent,
+                name: profileName[0].textContent,
                 posts: profileData[0].textContent,
                 followers: profileData[1].textContent,
                 following: profileData[2].textContent
             }});
+        }else{
+            // Delete previously cached data
+            chrome.storage.local.remove(['instaName', 'instaPosts', 'instaFollowers', 'instaFollowing']);
         }
     };
 

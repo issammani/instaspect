d1ade6c0d9a9ca37e43164a2cfe463252b97954d
issammani <issamouu69@gmail.com>
2020-05-12 17:22:55 +0000

    Add button controls and make popup.js a module

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,21 @@
+import requestLogger from './utils/requestLogger.js';
+
 chrome.storage.local.get(['instaName', 'instaPosts', 'instaFollowers', 'instaFollowing'], (response) => {
     if(Object.keys(response).length !== 0){
         document.querySelector('p').style.display = 'none';
         document.querySelector('.profile').style.display = 'block';
 
-        document.querySelector('.profile').innerHTML = `
+        document.querySelector('.profile').insertAdjacentHTML('afterbegin',`
             <p><b>Name:</b> ${response.instaName}</p>
             <p><b>Posts:</b> ${response.instaPosts}</p>
             <p><b>Followers:</b> ${response.instaFollowers}</p>
             <p><b>Following:</b> ${response.instaFollowing}</p>
-        `;
+        `);
     }
+});
+
+[...document.querySelectorAll('button')].forEach(button => {
+    button.addEventListener('click', (ev) => {
+        chrome.runtime.sendMessage({body: `get_${ev.target.id}`});
+    });
 });

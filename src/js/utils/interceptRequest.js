0e888ee7f64f6792c0ed2e1b11648e1af5a0f198
issammani <issamouu69@gmail.com>
2020-05-17 23:56:34 +0000

    Handle 429 too many requests error code

--- src/js/utils/interceptRequest.js
+++ src/js/utils/interceptRequest.js
@@ -4,10 +4,14 @@ const intercept = (url_match, callback) => {
     let send = XMLHttpRequest.prototype.send;
     XMLHttpRequest.prototype.send = function() {
         this.addEventListener('readystatechange', function() {
-        if (this.responseURL.includes(url_match) && this.readyState === 4) {
-            //callback(this);
-            callback(this.response || "");
-        }
+            if (this.responseURL.includes(url_match) && this.readyState === 4 && this.status === 200) {
+                callback(this.response || JSON.stringify({}));
+            }else if(this.responseURL.includes(url_match) && this.readyState === 4 && this.status === 429){
+                // Instagram limits the number of requests to 200 per hour
+                callback({error: 'request_limit_reached'});
+            }else{
+                // callback(JSON.stringify({})); // DO NOTHING !
+            }
         }, false);
         send.apply(this, arguments);
     };

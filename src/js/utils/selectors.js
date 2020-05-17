79421d251cc14d5edd3d22a2c90f6594cca3f207
issammani <issamouu69@gmail.com>
2020-05-17 23:06:44 +0000

    Add selector for profile modal

--- src/js/utils/selectors.js
+++ src/js/utils/selectors.js
@@ -2,4 +2,5 @@ export default {
     PROFILE_COUNT_SELECTOR : 'header section ul li',
     PROFILE_NAME_SELECTORS: ['header section h1', 'header section h2'],
     PROFILE_CONNECTIONS_SELECTOR : 'a[title]:not([data-fetched])',
+    CURRENTLY_SHOWN_MODAL : 'div[role="presentation"] ul' // Should find a better solution
 };
